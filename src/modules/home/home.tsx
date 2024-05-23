d4996384f31433af2e2edb6ad229475caf382054
Andres Lora <jalc.andres.lora@gmail.com>
2024-05-23 12:57:10 -0700

    Add footer component into home

--- src/modules/home/home.tsx
+++ src/modules/home/home.tsx
@@ -1,9 +1,10 @@
+import Footer from "@/shared/widgets/footer";
 import Header from "@/shared/widgets/header/header";
-import Banner from "./features/banner";
-import Benefits from "./features/benefits";
-import Branding from "./features/branding";
-import FeatureHighlight from "./features/feature.highlight";
-import Pricing from "./features/pricing";
+import Banner from "./elements/banner";
+import Benefits from "./elements/benefits";
+import Branding from "./elements/branding";
+import FeatureHighlight from "./elements/feature.highlight";
+import Pricing from "./elements/pricing";
 
 const Home = () => {
   return (
@@ -14,6 +15,7 @@ const Home = () => {
       <Benefits />
       <FeatureHighlight />
       <Pricing />
+      <Footer />
     </div>
   );
 };
